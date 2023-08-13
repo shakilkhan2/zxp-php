131cf5edbb6a9a56966dc1af935c7ad8c392ab5d
shakilkhan2 <dev.shakilkhan.com>
2023-08-14 01:27:43 +0600

    condition

--- index.php
+++ index.php
@@ -6,16 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-   hello php
+ 
    <?php
-echo "Hello, World!";
+echo "Hello, World!" . "<br>";
 
 echo "Bangladesh";
 ?> 
 <?php
-echo "Zepto X Programming Hero" . "\n";  
+echo "Zepto X Programming Hero" . "<br>";  
 
-echo "practice makes a man perfect.";
+echo "practice makes a man perfect." . "<br>";
 
 
 $work = "walking";
@@ -28,7 +28,7 @@ $subject = array("Math", "English", "Bangla");
 
 echo "$name is $work";
 echo "Nibir is $Work";
-echo "$name is $age years old. His CGPA is: $CGPA. And his subjects are " . implode(", ", $subject);
+echo "$name is $age years old. His CGPA is: $CGPA. And his subjects are " . implode(", ", $subject) . "<br>";
 
 echo "$name is ";
 if ($isStudent) {
@@ -38,7 +38,36 @@ if ($isStudent) {
 }
 echo ".\n";
 
-echo "$name is $age years old. His CGPA is: $CGPA. And his subjects are: " . implode(", ", $subject) . ".";
+echo "$name is $age years old. His CGPA is: $CGPA. And his subjects are: " . implode(", ", $subject) . "<br>";
+
+// 
+
+$location = "Dhaka";
+$opportunity = "Front-End Developer";
+
+if($location = "Dhaka"){
+    echo "There is a vacancy for $opportunity" . "<br>";
+}
+else{
+echo "There is no opportunity.";
+}
+
+// 
+
+$age = 25;
+$isStudent = true;
+
+if ($age >= 18) {
+    echo "You are eligible for something.";
+    if ($isStudent) {
+        echo " You're also a student!";
+    } else {
+        echo " You're not a student.";
+    }
+} else {
+    echo "Sorry, you're not eligible for this.";
+}
+
 ?>
 
 
