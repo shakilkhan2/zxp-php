181b203316403683a3074bea32a61232e906052f
shakilkhan2 <dev.shakilkhan.com>
2023-08-13 23:38:40 +0600

    learning php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   hello php
+   <?php
+echo "Hello, World!";
+
+echo "Bangladesh";
+?> 
+<?php
+echo "Zepto X Programming Hero";  
+
+echo "practice makes a man perfect.";
+
+
+$work = "walking";
+$Work = "Running";
+
+echo "Hasib is $work";
+echo "Nibir is $Work"
+?>
+</body>
+</html>
